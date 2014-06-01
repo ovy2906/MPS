ad60b6fab0a0827f9a329862a7df56486d90e4c6
iordi <i0rdy@yahoo.com>
2014-06-02 00:20:31 +0300

    revert2
    revert2

--- f1.c
+++ f1.c
@@ -4,11 +4,5 @@ int main () {
 	int a;
 	scanf("%d", &a)
   return 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-  //comentariu1
-=======
->>>>>>> upstream/master
-=======
->>>>>>> parent of 6bd8445... code review
+
 }
--- f2.cpp
+++ f2.cpp
@@ -7,12 +7,4 @@ class MPS {
 		for(int i=0; i<x ; i++)
 			printf("change!");
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-	//comentariu1
-	//comentariu2
-=======
->>>>>>> upstream/master
-=======
->>>>>>> parent of 6bd8445... code review
 };
